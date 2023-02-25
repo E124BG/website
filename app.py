8371740a149886ee7c8ff24e6540723612f794ac
E124BG <graak@live.fr>
2023-02-25 10:25:34 +0100

    container split into 2 columns added a lottie animation to the left

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+# LottieFile is an animation that scales without any pixelization
+
+lottie_cards_url = "https://assets6.lottiefiles.com/packages/lf20_LkyI64BhN0.json"
 
 #use of all of the width of the screen (standard would center the content)
 #emojis cheat sheet : https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -6,20 +12,36 @@ import streamlit as st
 
 st.set_page_config(page_title = "E124BG's Webpage", page_icon = ":tophat:", layout = "wide")
 
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:# we get response code 200 in case of successful get
+        return None
+    return r.json()
+            
+            
+# ---- LOAD ASSETS ----
+
+lottie_animation = load_lottie_url(lottie_cards_url)
+    
+
 # ---- HEADER SECTION ----
-st.subheader("Hi, I am Eliott :wave:")
-st.subheader("A Master in Computer Science student from Uni Lu :flag-lu:")
 
-st.write("I am passionate about software engineering, optimization and the applications of AI.")
-st.write(
-    """
-    Some of my current projects :  
-    - Deus ex Machina, an AI and ART project http://deus-x-machina.org/
-    - Rust by the book, a collection of short rust programs to learn rust step by step https://github.com/E124BG/rust-by-the-book
-    - A study of chatgpt and its usage in Luxembourg (link yet to come)
-    """)
-st.write("Contacts : [Linkedin](www.linkedin.com/in/eliott-bonte-534a76207)")
+with st.container():# a multi element container
+    left_column, right_column = st.columns(2)# split into 2 columns
+    with left_column:
+        st.subheader("Hi, I am Eliott :wave:")
+        st.subheader("A Master in Computer Science student from Uni Lu :flag-lu:")
 
-# LottieFile is an animation that scales without any pixelization
+        st.write("I am passionate about software engineering, optimization and the applications of AI.")
+        st.write(
+            """
+            Some of my current projects :  
+            - Deus ex Machina, an AI and ART project http://deus-x-machina.org/
+            - Rust by the book, a collection of short rust programs to learn rust step by step https://github.com/E124BG/rust-by-the-book
+            - A study of chatgpt and its usage in Luxembourg (link yet to come)
+            """)
+        st.write("Contacts : [Linkedin](www.linkedin.com/in/eliott-bonte-534a76207)")
 
-lottie_cards_url = "https://assets6.lottiefiles.com/packages/lf20_LkyI64BhN0.json"
+    with right_column:
+        st_lottie(lottie_animation, height= 300, key= "animation")
+
